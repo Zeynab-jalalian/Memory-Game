946e209c048f97c6c0a6d1dfeac03053a8fdbf97
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-09-20 10:55:19 +0330

    css

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let secondCard = false;
 const items = [
   { name: "girl", image: "img/girl.jpg" },
   { name: "scared", image: "img/scared.jpg" },
-  { name: "Screem", image: "img/Screem.jpg" },
+  { name: "Screem", image: "img/screem.jpg" },
   { name: "last", image: "img/last.jpg" },
   { name: "night", image: "img/night.jpg" },
   { name: "seat", image: "img/seat.jpg" },
@@ -22,7 +22,7 @@ const items = [
   { name: "man", image: "img/man.webp" },
   { name: "Picasso", image: "img/Picasso.jpg" },
   { name: "mona", image: "img/mona.jpg" },
-  { name: "Girl2", image: "img/Girl2.jpg" },
+  { name: "girl2", image: "img/girl2.jpg" },
   { name: "man2", image: "img/man2.jpg" },
 ];
 
@@ -73,8 +73,18 @@ const matrixGenerator=(cardValues,size=4)=>{
     //shuffle
     cardValues.sort(()=>Math.random() -0.5);
     for(let i=0;i<size*size;i++){
-
+     //before and after(image) card
+     gameContainer.innerHTML+=`
+     <div class="card-container" data-card-value="${cardValues[i].name}">
+     <div class="card-before">?</div>
+     <div class="card-after">
+     <img src="${cardValues[i].image}" class="image"></div>
+     </div>
+     </div>
+     `;
     }
+    //Grid
+    gameContainer.style.gridTemplateColumns=`repeat(${size},auto)`;
 }
 
 //initialize
